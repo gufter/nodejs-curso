b91ad8343c638f5d3846b675f518e2a4031f54a6
Gufter <gustavofq.1998@gmail.com>
2019-12-24 13:30:15 -0300

    Using mongoose and creating a database

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
 
 // Iniciando o DB
-mongoose.connect("mongodb://localhost:27017/nodeapi", {
-    useNewUrlParser: true
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
 
 // Primeira rota
-app.get("/", (req, res) => {
-    res.send("Hello YOOOOUUUU!!");
+app.get('/', (req, res) => {
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'http://github.com/facebook/react-native'
+  });
+
+  return res.send('Hello YOOOOUUUU!!');
 });
 
 app.listen(3001);
